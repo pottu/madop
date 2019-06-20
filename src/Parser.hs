a762132e1ef70b8bcea49534b7484608eea30833
Pontus Ernstedt <pontus_ponta@hotmail.com>
2019-06-20 11:58:00 +0200

    Intial (haddock) doc

--- src/Parser.hs
+++ src/Parser.hs
@@ -7,6 +7,8 @@ module Parser
 import qualified Text.Parsec as Prsc
 import Text.Parsec.String (Parser)
 
+-- TODO: Move types to separate file.
+-- | Represents a documents element structure.
 type Document = [Block]
 
 data Block 
@@ -18,6 +20,7 @@ data Span
     deriving(Show)
 
 
+-- | Parse a string formatted with Markdown.
 parseMd :: String -> Document
 parseMd s = let parsed = Prsc.parse parseBlock "" s 
              in case parsed of
