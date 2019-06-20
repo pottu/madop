587c54a2ac5df78bf1c88891dd43f1b0e247f2cb
Pontus Ernstedt <pontus_ponta@hotmail.com>
2019-06-20 11:57:25 +0200

    Handle missing arg

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,10 @@ import Parser
 
 main :: IO ()
 main = do
-    -- TODO: Handle multiple/invalid args
-    mdToParse <- getArgs
-    print $ parseMd $ head mdToParse
+    -- TODO: Handle multiple/invalid args"
+    args <- getArgs
+    if null args
+    then putStrLn "Please supply string to be parsed."
+    else do
+        mdToParse <- getArgs
+        print $ parseMd $ head mdToParse
