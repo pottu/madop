4424484b9f7091df9719b56ac341a9cea68ce432
Pontus Ernstedt <pontus_ponta@hotmail.com>
2019-06-25 11:03:40 +0200

    Handle choice between parsers

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,6 +1,7 @@
 module Parser (parseMd) where
 
 import Types
+import Control.Applicative ((<|>))
 import qualified Text.Parsec as Prsc
 import Text.Parsec.String (Parser)
 
@@ -13,17 +14,16 @@ parseMd s = let parsed = Prsc.parse parseBlock "" s
                   Left e -> error (show e)
 
 
--- FIXME: This is just a dummy.
 parseBlock :: Parser Block
-parseBlock = do
-    text <- Prsc.many Prsc.anyChar
-    return $ Paragraph $ [Text text]
-
+parseBlock = 
+    Prsc.try parseHeader
+    <|> parseParagraph
 
 
 -- FIXME: Assumes EOL (as in: won't handle EOF)
 --        Maybe append EOL to input at start.
 -- TODO: Handle Setext-style headers.
+-- FIXME: Parse span elements.
 parseHeader :: Parser Block
 parseHeader = do
     level <- length <$> Prsc.many1 (Prsc.char '#')
@@ -36,3 +36,10 @@ parseHeader = do
                 Prsc.skipMany (Prsc.char '#')
                 Prsc.skipMany (Prsc.char ' ')
                 Prsc.endOfLine
+
+
+-- FIXME: This is just a dummy.
+parseParagraph :: Parser Block
+parseParagraph = do
+    text <- Prsc.manyTill Prsc.anyChar (Prsc.oneOf "\n")
+    return $ Paragraph [Text text]
