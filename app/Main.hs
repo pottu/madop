af4659cd5bb2a615229c645c0b206421f143e0b4
Pontus Ernstedt <pontus_ponta@hotmail.com>
2019-06-26 14:53:21 +0200

    Now gets input from file only

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,18 @@ import System.Environment (getArgs)
 import Parser 
 import Renderer
 
+-- TODO: Handle multiple/invalid args
+-- FIXME: Better error msg when trouble opening file.
 main :: IO ()
 main = do
-    -- TODO: Handle multiple/invalid args"
-    args <- getArgs
-    if null args
-    then putStrLn "Please supply string to be parsed."
-    else do
-        mdToParse <- getArgs
-        putStrLn $ renderHTML $ parseMd $ head mdToParse
+  args <- getArgs
+  if null args
+     then
+       putStrLn "Please supply a file to be parsed."
+     else do
+       file <- readFile $ head args
+       putStrLn $ renderHTML $ parseMd file
+
+
+
+
