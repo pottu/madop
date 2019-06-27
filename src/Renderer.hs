e2811d248a2e1fd4ff8a8a2e5baadcdb3b547a44
Pontus Ernstedt <pontus_ponta@hotmail.com>
2019-06-27 17:54:57 +0200

    Renders spans and headers

--- src/Renderer.hs
+++ src/Renderer.hs
@@ -7,6 +7,10 @@ renderHTML = concatMap renderBlock
 
 renderBlock :: Block -> String
 renderBlock (Paragraph content) = "<p>" ++ renderSpans content ++ "</p>"
+renderBlock (Header level spans) = 
+  -- FIXME: Ensure level <= 6!
+  let content = renderSpans spans
+   in "<h" ++ show level ++ ">" ++ content ++ "</h" ++ show level ++ ">"
 -- Add cases when Block expands.
 
 renderSpans :: [Span] -> String
@@ -14,5 +18,8 @@ renderSpans = concatMap renderSpan
 
 renderSpan :: Span -> String
 renderSpan (Text s) = s
+renderSpan (Space) = " "
+renderSpan (Link text href title) =
+  "<a href=\"" ++ href ++ "\" title=\"" ++ title ++ "\">" ++ text ++ "</a>"
 -- Add cases when Span expands.
 
