483d6adfdb4f78df90502e4d153fd70ccfdb4a5f
Pontus Ernstedt <pontus_ponta@hotmail.com>
2019-07-17 15:59:00 +0200

    Add simple options + program functionality

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,86 @@
 module Main where
 
 import System.Environment (getArgs)
-import Parser 
-import Renderer
+import Parser (parseMd)
+import Renderer (renderHTML)
+import Text.Parsec as Prsc
+import Text.Parsec.String (Parser)
+
+data Input = File String | Stdin 
+
+data Exec = Default Input 
+          | Output String Input 
+          | Help
+
+usageMsg = "A simple Markdown to HTML converter.\n\
+           \Usage: madop [-o output_file] (--stdin | input_file)\n\
+           \       madop [-h|--help]\n\n\
+           \Options:\n\
+           \  -h | --help  Display this message\n\
+           \  -o <file>    Place the output into <file>\n\
+           \  --stdin      Get input from stdin instead of a file"
 
 -- TODO: Handle multiple/invalid args
 -- FIXME: Better error msg when trouble opening file.
 main :: IO ()
 main = do
   args <- getArgs
-  if null args
-     then
-       putStrLn "Please supply a file to be parsed."
-     else do
-       file <- readFile $ head args
-       putStrLn $ renderHTML $ parseMd file
+  let parsed = Prsc.parse parseArgs "" (unwords args)
+   in either (error . show) execute parsed
+  where
+    execute (Default (File file)) = do
+      input <- readFile file
+      putStr $ renderHTML $ parseMd input
+
+    execute (Default Stdin) = do
+      input <- getContents
+      putStr $ renderHTML $ parseMd input
+
+    execute (Output outfile (File file)) = do
+      input <- readFile file 
+      writeFile outfile (renderHTML (parseMd input))
+
+    execute (Output outfile Stdin) = do
+      input <- getContents
+      writeFile outfile (renderHTML (parseMd input))
+
+    execute Help = putStrLn usageMsg
+
+
+parseArgs :: Parser Exec 
+parseArgs = Prsc.try output
+        <|> Prsc.try help
+        <|> pDefault
+        <|> return Help
+
+pDefault :: Parser Exec
+pDefault = do
+  input <- (Prsc.try stdinInput) <|> fileInput
+  return $ Default input 
+  
+
+output :: Parser Exec
+output = do
+  Prsc.string "-o"
+  Prsc.many1 $ Prsc.char ' '
+  outfile <- Prsc.manyTill Prsc.anyChar (Prsc.char ' ')
+  input <- (Prsc.try stdinInput) <|> fileInput
+  return $ Output outfile input 
+
+help :: Parser Exec
+help = do
+  Prsc.try (Prsc.string "-h") <|> Prsc.string "--help"
+  return Help
+
+stdinInput :: Parser Input 
+stdinInput = do
+  Prsc.string "--stdin"
+  return $ Stdin 
+  
+fileInput :: Parser Input 
+fileInput = do
+  file <- Prsc.many1 Prsc.anyChar 
+  return $ File file
 
 
 
