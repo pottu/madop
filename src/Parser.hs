d1678097f129d7b99a58a1dcc3b07d490a2e7a8b
Pontus Ernstedt <pontus_ponta@hotmail.com>
2019-07-22 15:37:02 +0200

    Add html element names

--- src/Parser.hs
+++ src/Parser.hs
@@ -14,6 +14,19 @@ type Parser = Prsc.Parsec String ParserState
 
 mdSymbols = ['-', '*', '_', '[', ']', '(', ')', '#', '`', '!']
 
+-- https://www.w3schools.com/html/html_blocks.asp
+htmlBlocks = 
+  ["address", "article", "aside", "blockquote", "canvas", "dd", "div", "dl",
+  "dt", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3",
+  "h4", "h5", "h6", "header", "hr", "li", "main", "nav", "noscript", "ol", "p",
+  "pre", "section", "table", "tfoot", "ul", "video"]
+
+htmlSpans =
+  ["a", "abbr", "acronym", "b", "bdo", "big", "br", "button", "cite", "code",
+  "dfn", "em", "i", "img", "input", "kbd", "label", "map", "object", "output",
+  "q", "samp", "script", "select", "small", "span", "strong", "sub", "sup",
+  "textarea", "time", "tt", "var"]
+
 
 
 -- | Parse a string formatted with Markdown.
